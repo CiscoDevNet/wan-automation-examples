11d88fa3a1f51cfe246ac8e53ae704008da26b0c
Roland Saville (rsaville) <rsaville@cisco.com>
2025-11-14 13:33:41 -0500

    Additions to meraki/terraform for consistency with catalystwan/terraform

--- meraki/terraform/module/main.tf
+++ meraki/terraform/module/main.tf
@@ -12,7 +12,7 @@ provider "meraki" {
 module "meraki" {
   # source = "netascode/nac-sdwan/sdwan"
   source = "git::https://github.com/netascode/terraform-meraki-nac-meraki.git?ref=main"
-  # version = "0.4.0"
+  # version = "0.5.0"
 
   yaml_directories = ["data/"]
 
